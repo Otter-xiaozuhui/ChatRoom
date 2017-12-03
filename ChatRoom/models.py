ea69337e5cc938a4a24076bdaabebbecbe11899a
xiaozuhui <1143841809@qq.com>
2017-12-03 23:39:43 +0800

    还差一个房间模型，2017-12-3 23:39

--- ChatRoom/models.py
+++ ChatRoom/models.py
@@ -18,3 +18,26 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class Room(models.Model):
+    pass
+
+
+class Message(models.Model):
+    from_user = models.ForeignKey(User, verbose_name="发送信息的用户")
+    # 因为是房间群发，所以没必要在意是谁什么时候接受到了信息，但依旧再次保留
+    # to_user = models.ForeignKey(User, verbose_name="接受信息的用户")
+    send_time = models.DateTimeField(verbose_name="发送时间")
+    room = models.ForeignKey(Room)
+    # get_time = models.DateTimeField(verbose_name="接受时间")
+    content = models.TextField(verbose_name="信息内容")
+
+    class Meta:
+        verbose_name = "信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.content
+
+
