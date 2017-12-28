b0b4604d32cb763207700b5442237fa8223b28cf
xiaozuhui <1143841809@qq.com>
2017-12-28 08:10:05 -0800

    2017-12-29 00:09

--- chatroom/models.py
+++ chatroom/models.py
@@ -17,7 +17,7 @@ class User(models.Model):
 
 class Room(models.Model):
     room_name = models.CharField(max_length=20, verbose_name="房间名称", default="未命名")
-    master = models.ForeignKey(User, verbose_name="房主", related_name='master')
+    master = models.ForeignKey(User, verbose_name="房主", related_name='master',on_delete=models.DO_NOTHING)
     users = models.ManyToManyField(User, verbose_name="用户", related_name='chaters')
     create_time = models.DateTimeField(verbose_name="创建时间")
     room_password = models.CharField(verbose_name="房间密码", max_length=120, null=True)
@@ -32,11 +32,11 @@ class Room(models.Model):
 
 
 class Message(models.Model):
-    from_user = models.ForeignKey(User, verbose_name="发送信息的用户")
+    from_user = models.ForeignKey(User, verbose_name="发送信息的用户", on_delete=models.DO_NOTHING)
     # 因为是房间群发，所以没必要在意是谁什么时候接受到了信息，但依旧再次保留
     # to_user = models.ForeignKey(User, verbose_name="接受信息的用户")
     send_time = models.DateTimeField(verbose_name="发送时间")
-    room = models.ForeignKey(Room)
+    room = models.ForeignKey(Room, on_delete=models.DO_NOTHING)
     # get_time = models.DateTimeField(verbose_name="接受时间")
     content = models.TextField(verbose_name="信息内容")
 
